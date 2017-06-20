58ecd581955d1ded73da86a584fd99865942321a
meryldakin <meryl.k.dakin@gmail.com>
2017-06-20 18:31:05 -0400

    cosmetic changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     user = User.find(params[:current_user_id])
     friend = User.find(params[:friend_id])
     event_list = params[:event_list_id] ? EventList.find(params[:event_list_id]) : EventList.find(2)
-    gift = Gift.create(item: params[:item], category: params[:category], image: params[:image], link: params[:link], user: user)
+    gift = Gift.create(item: params[:item], image: params[:image], link: params[:link])
     user.create_exchange(friend.id, gift.id, event_list.id)
     render json: user.friendships
   end
@@ -54,6 +54,8 @@ class UsersController < ApplicationController
   def delete_gift
     user = User.find(params[:current_user_id])
     user.delete_exchange(params[:exchange_id])
+    events = EventList.where(user: user)
+    events_and_celebrations = events.includes(:celebrations)
     render json: user.friendships
   end
 
--- app/models/gifts_event.rb
+++
@@ -1,4 +0,0 @@
-class GiftsEvent < ApplicationRecord
-  belongs_to :gift
-  belongs_to :event_list
-end
